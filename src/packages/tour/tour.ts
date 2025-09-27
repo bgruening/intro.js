07673be076c421d99b2cef560a85d4312c108dd5
Parvin <parvinmh74@gmail.com>
2025-09-27 15:51:01 +0300

    remove duplicate code

--- src/packages/tour/tour.ts
+++ src/packages/tour/tour.ts
@@ -12,11 +12,6 @@ import {
 } from "./callback";
 import { getDefaultTourOptions, TourOptions } from "./option";
 import { setOptions, setOption } from "../../option";
-import {
-  LanguageCode,
-  getLanguageByCode,
-  Translator,
-} from "../../i18n/language";
 import { start } from "./start";
 import exitIntro from "./exitIntro";
 import isFunction from "../../util/isFunction";
@@ -304,46 +299,33 @@ export class Tour implements Package<TourOptions> {
    * @param partialOptions key/value pair of options
    */
   setOptions(partialOptions: Partial<TourOptions>): this {
-    // Handle language code conversion and update button labels
-    const processedOptions = { ...partialOptions };
+    if (partialOptions.language) {
+      // Use getDefaultTourOptions to get properly translated labels
+      const tempOptions = getDefaultTourOptions(partialOptions.language);
 
-    if (processedOptions.language) {
-      let languageObj;
-      if (typeof processedOptions.language === "string") {
-        languageObj = getLanguageByCode(
-          processedOptions.language as LanguageCode
-        );
-        processedOptions.language = languageObj;
-      } else {
-        languageObj = processedOptions.language;
-      }
+      // Only override labels that weren't explicitly provided
+      const languageUpdates: Partial<TourOptions> = {
+        language: tempOptions.language,
+      };
 
-      // Update button labels based on the new language
-      const translator = new Translator(languageObj);
+      if (!partialOptions.nextLabel)
+        languageUpdates.nextLabel = tempOptions.nextLabel;
+      if (!partialOptions.prevLabel)
+        languageUpdates.prevLabel = tempOptions.prevLabel;
+      if (!partialOptions.doneLabel)
+        languageUpdates.doneLabel = tempOptions.doneLabel;
+      if (!partialOptions.stepNumbersOfLabel)
+        languageUpdates.stepNumbersOfLabel = tempOptions.stepNumbersOfLabel;
+      if (!partialOptions.dontShowAgainLabel)
+        languageUpdates.dontShowAgainLabel = tempOptions.dontShowAgainLabel;
 
-      // Only update labels if they weren't explicitly provided in the options
-      if (!partialOptions.nextLabel) {
-        processedOptions.nextLabel = translator.translate("buttons.next");
-      }
-      if (!partialOptions.prevLabel) {
-        processedOptions.prevLabel = translator.translate("buttons.prev");
-      }
-      if (!partialOptions.doneLabel) {
-        processedOptions.doneLabel = translator.translate("buttons.done");
-      }
-      if (!partialOptions.stepNumbersOfLabel) {
-        processedOptions.stepNumbersOfLabel = translator.translate(
-          "messages.stepNumbersOfLabel"
-        );
-      }
-      if (!partialOptions.dontShowAgainLabel) {
-        processedOptions.dontShowAgainLabel = translator.translate(
-          "messages.dontShowAgainLabel"
-        );
-      }
+      this._options = setOptions(this._options, {
+        ...partialOptions,
+        ...languageUpdates,
+      });
+    } else {
+      this._options = setOptions(this._options, partialOptions);
     }
-
-    this._options = setOptions(this._options, processedOptions);
     return this;
   }
 
